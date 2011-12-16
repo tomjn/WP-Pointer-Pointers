414b2c9297572b1c49da16551e20398da6b090ea
Tom Nowell <tarendai@darkstars.co.uk>
2011-12-16 11:56:14 +0000

    Indent one block by 1 level

--- wp-pointer-pointer.php
+++ wp-pointer-pointer.php
@@ -13,23 +13,23 @@ function meta_pointer(){
 	?>
 	<script>
 	jQuery(document).ready( function($) {
-	setTimeout(function (){
+		setTimeout(function (){
 
-		if($.pointer){
+			if($.pointer){
 
-			$('.wp-pointer').pointer({
-				content: '<h3>New Feature: Pointers</h3><p>We have added a new feature called Pointers to point out new features in your admin dashboard.</p>',
-				position: {
-					my: 'left top',
-					at: 'center bottom',
-					offset: '-25 0'
-				},
-				close: function() {
-					setUserSetting( 'p1', '1' );
-				}
-			}).pointer('open');
-		}
-	},300);
+				$('.wp-pointer').pointer({
+					content: '<h3>New Feature: Pointers</h3><p>We have added a new feature called Pointers to point out new features in your admin dashboard.</p>',
+					position: {
+						my: 'left top',
+						at: 'center bottom',
+						offset: '-25 0'
+					},
+					close: function() {
+						setUserSetting( 'p1', '1' );
+					}
+				}).pointer('open');
+			}
+		},300);
 
 	});</script><?php
 }
